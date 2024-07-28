212b92266c0c41b4e179fcdd8e29e7c4f5999912
archengdev <arc.albertcheng@gmail.com>
2024-07-29 00:11:51 +0800

    finished solver loop

--- sweeper.py
+++ sweeper.py
@@ -1,49 +1,14 @@
 from utils import *
-import cProfile
+import keyboard
 
-
-# pyautogui.alert("Click OK to start")
-# pyautogui.click(1300,800)
-# time.sleep(0.3)
-
-# def foo():
-#     board = init_board()
-#     board = scan(board)
-
-# cProfile.run('foo()')
-
-# board = init_board()
-# board = scan(board)
-# ref, mat, dic = create_mat(board)
-# find_mines(ref, mat, board, dic)
-# print(ref)
-
-
-
-# print(mat)
-# print(dic)
-# for i in mat:
-#     if i[46] != 0: 
-#         print(i)
-#         # ffind_mines(i)
-#         pass
-
-# print(dic[(1,5)])
-# find_mines(mat, board, dic)
-# board = init_board()
-
-for i in range(50):
-    # pyautogui.screenshot(str(i) + '.png')
+while True:
+    # create board, then scan board, then make and use matrix to clear board
     board = init_board()
     scan(board)
-    # print()
-    print_board(board)
-    # print()
     mat, dic = create_mat(board)
-    print(mat, dic)
     find_mines(mat, board, dic)
-    pyautogui.moveTo(30,30)
-    time.sleep(0.9)
 
-
-#TODO: cut off complete squares, include regular echelon matrix for some situations?
+    # pause to let animations finish
+    time.sleep(0.6)
+    if keyboard.is_pressed('q'):
+        break
