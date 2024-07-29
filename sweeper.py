72a166e321e02db62052cd8a15d09486bfa9cf41
archengdev <arc.albertcheng@gmail.com>
2024-07-29 20:41:13 +0800

    increased pause time

--- sweeper.py
+++ sweeper.py
@@ -1,6 +1,8 @@
 from utils import *
 import keyboard
 
+time.sleep(0.7)
+
 while True:
     # create board, then scan board, then make and use matrix to clear board
     board = init_board()
@@ -9,6 +11,6 @@ while True:
     find_mines(mat, board, dic)
 
     # pause to let animations finish
-    time.sleep(0.6)
+    time.sleep(0.7)
     if keyboard.is_pressed('q'):
         break
